1b1241dc49d533a894b41aad346be234dfeb9d6e
isaacbrds <isaac.brigido@gmail.com>
2022-11-07 11:34:38 -0300

    feat: Criando metodo contrutor da classe e herdando de outra classe no curso de ruby

---
+++ curso_ruby/construtor.rb
@@ -0,0 +1,26 @@
+class Produto
+
+  def self.vencimento
+    'Produto dentro do prazo de validade'
+  end
+
+  def initialize(nome, preco)
+    @nome = nome
+    @preco = preco
+  end
+
+  def mostra_produto
+    "O #{@nome} está custando R$ #{@preco}"
+  end
+end
+
+#Herança
+class Limpeza < Produto
+  def initialize(nome, preco)
+    super
+  end
+end
+
+limpeza = Limpeza.new('Kiboa', 1.50)
+puts Limpeza.vencimento
+puts limpeza.mostra_produto
