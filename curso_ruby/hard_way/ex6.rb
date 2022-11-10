77b3704c2af38541cd0f589caf91fbe68f8474a7
isaacbrds <isaac.brigido@gmail.com>
2022-11-10 07:54:01 -0300

    feat: Resolvendo exercicios 7,8,9 do learn ruby the hard way

--- curso_ruby/hard_way/ex6.rb
+++ curso_ruby/hard_way/ex6.rb
@@ -8,3 +8,16 @@ y = "Those who know #{binary} are those who #{do_not}"
 
 puts x 
 puts y
+
+puts "I said: #{x}"
+puts "I also said: '#{y}'."
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+puts joke_evaluation
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+puts w + e
