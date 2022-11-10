a2bf59ec7978fca65dcaa8bc83914912bec7e053
isaacbrds <isaac.brigido@gmail.com>
2022-11-10 07:11:46 -0300

    feat: Resolvendo exercicios 4,5,6 do learn ruby the hard way

---
+++ curso_ruby/hard_way/ex6.rb
@@ -0,0 +1,10 @@
+type_of_people = 10
+x = "There are #{type_of_people} types of people."
+
+binary = "binary"
+do_not = "don't"
+
+y = "Those who know #{binary} are those who #{do_not}"
+
+puts x 
+puts y
